27e09b8eda04bea8b1378102665f6a44e2626ae9
Andi-Bogdan Postelnicu <abpostelnicu@gmail.com>
2017-11-09 13:45:31 +0100

    Code cleanup.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::io::BufReader;
 use std::collections::VecDeque;
 
 struct Redish {
-    pub listener: TcpListener,
     pub messages : VecDeque<String>,
 }
 
@@ -14,44 +13,94 @@ impl Redish {
     fn handle_client(&mut self, mut stream: TcpStream) {
         // ...
         println!("Connected");
-        println!("Messages Length: {}", self.messages.len());
         let mut data = String::new();
-        let mut buffer = BufReader::new(stream.try_clone().unwrap());
-        buffer.read_line(&mut data);
-        println!("Command received: {}", data);
-        //
 
-        if (data == "Get") {
-            println!("Get Something");
-            let elem = self.messages.get(0).unwrap();
+        // clone stream
+        match stream.try_clone() {
+            Ok(cloned_stream) => {
+                let mut buffer = BufReader::new(cloned_stream);
 
-            stream.write(elem.as_bytes());
-        }
-        else {
-            let vec: Vec<String> = data.split(" ").map(|s| s.to_string()).collect();
-            if vec.len() == 2 && vec.get(0).unwrap() == "Put" {
-                let str2put = vec.get(1).unwrap().to_string();
-                println!("Put {}", str2put);
-                self.messages.push_back(str2put);
+                match buffer.read_line(&mut data) {
+                    Ok(_data_size) => {
+                        let vec: Vec<String> = data.split(" ").map(|s| s.to_string()).collect();
+                        match vec.get(0) {
+                            Some(action) => {
+                                println!("Command received: {}", action);
+
+                                if action == "Get" {
+                                    println!("Get Something");
+                                    match self.messages.get(0) {
+                                        Some(elem) => {
+                                            match stream.write(elem.as_bytes()) {
+                                                Ok(_value) => {
+                                                    // success do nothing
+                                                }
+                                                Err(_e) => {
+                                                    return;
+                                                }
+                                            }
+                                        }
+                                        None => {
+                                            return;
+                                        }
+                                    }
+                                }
+                                else if action == "Put" {
+                                    let str2put = vec.get(1);
+                                    match str2put {
+                                        Some(str2put) => {
+                                            let put2vec = str2put.to_string();
+                                            println!("Put {}", put2vec);
+                                            self.messages.push_back(put2vec);
+                                        }
+                                        None => {
+
+                                        }
+                                    }
+                                }
+                            }
+                            None => {
+                            }
+                        }
+                    }
+                    Err(_e) => {
+                        return;
+                    }
+                }
+            }
+            Err(_e) => {
+                return;
             }
         }
-        //else
+        println!("Messages Length: {}", self.messages.len());
     }
 
-    fn new(bind_to: String) -> Self {
+    fn new() -> Self {
         Redish {
-            listener: TcpListener::bind(bind_to).unwrap(),
             messages: VecDeque::default()
         }
     }
 }
 
 fn main() {
-    //let mut redish = Redish::new(String::from("127.0.0.1:8989"));
-    let mut redish = Redish::new(String::from("127.0.0.1:0000"));
-    let mut listener = TcpListener::bind("127.0.0.1:10000").unwrap();
-    //for stream in redish.listener.incoming() {
-    for stream in listener.incoming() {
-        redish.handle_client(stream.unwrap());
+    let mut redish = Redish::new();
+    let listener = TcpListener::bind("127.0.0.1:10000");
+    match listener {
+        Ok(listener) => {
+             //for stream in redish.listener.incoming() {
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        redish.handle_client(stream);
+                    }
+                    Err(_e) => {
+                        continue;
+                    }
+                }
+            }
+        }
+        Err(_e) => {
+            return;
+        }
     }
 }
